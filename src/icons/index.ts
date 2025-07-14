67e0177144ebad2593a7078f08bab44b1a0b90c6
Krzysztof Kryczka <kw.kryczka@gmail.com>
2025-07-14 19:19:19 +0200

    feat: added icons

---
+++ src/icons/index.ts
@@ -0,0 +1,6 @@
+import UserIcon from './UserIcon'
+import PhoneIcon from './PhoneIcon'
+import EmailIcon from './EmailIcon'
+import MessageIcon from './MessageIcon'
+
+export { UserIcon, PhoneIcon, EmailIcon, MessageIcon }
