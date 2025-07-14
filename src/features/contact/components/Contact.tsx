3f042e43deb9c4aaad8d42a8ac416a3fec04233d
Krzysztof Kryczka <kw.kryczka@gmail.com>
2025-07-14 21:20:38 +0200

    feat: Created Contact Form

---
+++ src/features/contact/components/Contact.tsx
@@ -0,0 +1,14 @@
+import { Topbar } from '@/features/layout/components'
+import ContactForm from './ContactForm'
+
+const Contact = () => {
+  return (
+    <div>
+      <Topbar />
+      <p>Strona Kontaktowa</p>
+      <ContactForm />
+    </div>
+  )
+}
+
+export default Contact
